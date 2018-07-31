27f5ef864b67df219145a0bae9f72c331f2f85f0
vladislav.tabala <vladislav.tabala@x2sy.com>
2018-07-31 18:12:51 +0300

    feat: test snapshots

--- app/containers/Friends/index.test.js
+++ app/containers/Friends/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-
+import renderer from 'react-test-renderer'
 import { Friends } from './index'
 import SideBar from '../../components/SideBar'
 import InputField from '../../components/InputField'
@@ -14,8 +14,14 @@ describe('Friends component', () => {
 
     const component = shallow(<Friends state={store} getUserId={() => {}} />);
 
-    it('render inputs', () => {
+    it('rendered Friends component', () => {
+        const component = renderer.create(<Friends state={store} getUserId={() => {}} />);
+        const tree = component.toJSON();
 
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('rendered inputs', () => {
         const redneredSideBar = component.find(SideBar);
         expect(redneredSideBar.find(InputField)).toHaveLength(3)
     });
--- app/containers/Home/index.test.js
+++ app/containers/Home/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-
+import renderer from 'react-test-renderer'
 import Authorization from '../../components/Authorization'
 import { LogIn, SaveToken } from '../../components/Authorization/styled'
 
@@ -8,16 +8,23 @@ describe('Authorization component', () => {
     const saveFn = jest.fn();
     const component = shallow(<Authorization token={null} onClick={saveFn}/>);
 
-    it('Render login link', () => {
+    it('rendered Authorization component', () => {
+        const component = renderer.create(<Authorization token={null} onClick={saveFn}/>);
+        const tree = component.toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('rendered login link', () => {
         expect(component.find(LogIn)).toHaveLength(1);
     });
 
-    it('Render save token button', () => {
+    it('rendered save token button', () => {
         component.setProps({token:'test'});
         expect(component.find(SaveToken)).toHaveLength(1);
     });
     
-    it('Should call function when click save token', () => {
+    it('should call function when click save token', () => {
         component.find(SaveToken).simulate('click');
         expect(saveFn).toHaveBeenCalled();
     });
